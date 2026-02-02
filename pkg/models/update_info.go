69ebf9512571d9569809f4c411bc8ac98378325f
allan716 <525223688@qq.com>
2026-02-02 10:58:20 +0800

    feat(models): 添加更新信息数据模型

---
+++ pkg/models/update_info.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+// UpdateInfo 更新信息
+type UpdateInfo struct {
+	HasUpdate      bool      `json:"hasUpdate"`      // 是否有更新
+	LatestVersion  string    `json:"latestVersion"`  // 最新版本号
+	CurrentVersion string    `json:"currentVersion"` // 当前版本号
+	ReleaseNotes   string    `json:"releaseNotes"`   // Release notes
+	PublishedAt    time.Time `json:"publishedAt"`    // 发布时间
+	DownloadURL    string    `json:"downloadURL"`    // 下载链接
+	AssetName      string    `json:"assetName"`      // 资源文件名
+	Size           int64     `json:"size"`           // 文件大小
+}
